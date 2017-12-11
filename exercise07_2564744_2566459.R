c674b2146ee8d928590ca4e6045d6d50e081cd2d
Alyona <17325652+uxfairy@users.noreply.github.com>
2017-12-11 23:32:29 +0100

    assignment 7 done

--- exercise07_2564744_2566459.R
+++ exercise07_2564744_2566459.R
@@ -22,15 +22,19 @@
 
 ###########################################################################################
 
-
-
 #######################
 ### PART 1: Preparation
 #######################
 
 library(boot)
+library(reshape2)
 library(ggplot2)
 library(reshape)
+library(tidyr)
+library(lsr)
+library(dplyr)
+library(stats)
+library(car)
 
 # This time we will be working with the "amis" data frame (package 'boot') that has 
 # 8437 rows and 4 columns.
@@ -46,37 +50,45 @@ library(reshape)
 # in place for some time.
 
 # For the further reference please use ?amis. It may take some time to understand the dataset. 
+
 ?amis
+
 # Load the dataset and briefly inspect it. 
 # Feel free to make some plots, calculate some statistics in order to understand the data.
-head(amis)
-str(amis)
+
+data<-boot::amis
+
+who(TRUE) #shows current structure of data
+head(data)
+str(data)
 
 # All our columns have numeric type. Convert the categorial columns to factors.
-names <- names(amis)
+names <- names(data)
 names <- names[2:4]
-amis[names] <- lapply(amis[names], factor)
+data[names] <- lapply(data[names], factor)
 
-levels(amis$period) <- c("before", "immediately", "after")
-levels(amis$warning) <- c("sign", "no sign")
+levels(data$period) <- c("before", "immediately", "after")
+levels(data$warning) <- c("sign", "no sign")
 
 # Build a boxplot for the distribution of `speed` for each of `period` values 
 # (before, immediately after and after some time). Build 2 plots side by side
 # depending on the `warning` variable.
 # (for all plots here and below please use ggplot)
-ggplot(amis, aes(x = period, y = speed)) +
+ggplot(data, aes(x = period, y = speed)) +
   geom_boxplot() + 
   facet_grid(~ warning)
 
 # What can you conclude according this plot? What can you say about people behaviour in
 # different periods: before, immediately after and after some time?
-# It can be concluded that the average speed increases after the sign. Immediately after the sign was put,
-# it decreases a bit and then increased more than before
+
+## It can be concluded that the average speed increases after the sign. Immediately after the sign was put,
+## it decreases a bit and then increased more than before
 
 # What are your ideas about why the data with warning==2 (which correspond to the
 # measurements in different times on sites where no sign was erected) was collected?
-# The data with no signs was collected in order to compare it with the data collected after the sign was erected.
-# This comparision will show if there was a real difference between these two variables.
+
+## The data with no signs was collected in order to compare it with the data collected after the sign was erected.
+## This comparision will show if there was a real difference between these two variables.
 
 
 #######################
@@ -84,7 +96,8 @@ ggplot(amis, aes(x = period, y = speed)) +
 #######################
 # For 1-way ANOVA we will be working with the subset of `amis` where the 
 # warning sign was erected, which corresponds to warning==1.
-amisdata <- subset()
+
+data_warn <- subset(data, warning == "sign")
 
 # First, let's check the ANOVA assumptions 
 # 1.) Independence assumption is violated, because we have repeated measurements from 
@@ -96,58 +109,141 @@ amisdata <- subset()
 # independence is violated due to multiple measurements per same subject (remember ex5) or 
 # per item (this exercise).
 
+data_warn_av <- dcast(data_warn, period + pair ~ 'avg_speed', mean, na.rm = T, value.var = "speed")
+head(data_warn_av)
 
 # Please, note that we will be working only with this new data frame in this section.
 # Again, build a boxplot of the average speed depending on period
 
+ggplot(data_warn_av, aes(x = period, y = avg_speed)) +
+  geom_boxplot()
 
 # How do you think if there is a difference between periods?
 
+## According to our new boxplot there is a difference between periods and it is visible more clear. 
+## The lowest average speed corresponds to the situation when a reading was taken shortly after erection of the sign  
+
+model <- lm(avg_speed ~ period + pair, data = data_warn_av)
 
 # 2.) Normality of residuals
 # (you need to figure out the best way to do it and give a detailed justified answer)
 
+hist(residuals(model))
+
+##doesn't look completely like a bell curve
+
+boxplot(residuals(model))
+
+##boxplot is non-symmetric, which probably tell us of non-normality of residuals
+
+shapiro.test(residuals(model))
+
+## but according to shapiro test though we cannot reject the hypothesis of normality of residuals, as p-value is relatively high
+
+qqnorm(residuals(model))
+qqline(residuals(model))
+
+##qq plot also looks quite "normal"
 
 # 3.) Homogeneity of variance of residuals
 # (you need to figure out the best way to do it and give a detailed justified answer)
 
+##we may use Bartlett test or Figner-Killeen test or Levene test
 
+bartlett.test(avg_speed ~ period, data = data_warn_av)
+fligner.test(avg_speed ~ period, data = data_warn_av)
+leveneTest(data_warn_av$avg_speed, data_warn_av$period) #requires "car" library
+
+##according to p-value that equals nearly 0.8 we cannot reject hypothesis of homogeneity of variance of residuals
 
 # Now we are ready to perform 1-way ANOVA: please use the function aov() on the speed
 # depending on the period,
 # report p-value and interpret the result in details
-
+anova_1_way <- aov(data_warn_av$avg_speed ~ data_warn_av$period)
+summary(anova_1_way)
 
 # Please do a pairwise t-test with pairwise.t.test()
+pairwise.t.test(data_warn_av$avg_speed, data_warn_av$period)
 
 # Report pair-wise p-values and interpret the result in details
+## F-value = 0.986, p-value = 0.382
+## F value is quite high, which means that variance between groups is way higher than variance witheen groups, that's how it should be
+## but high p-value tells of statistical insignificance
 
 
 # Try to use no adjustment for pairwise testing and then Bonferroni correction.
 # Does the result change?
+pairwise.t.test(data_warn_av$avg_speed, data_warn_av$period, p.adjust = "none")
+pairwise.t.test(data_warn_av$avg_speed, data_warn_av$period, p.adjust = "bonferroni")
 
-
+## results changed in a predictable way, as Bonferroni is the most conservative method, while no adjustment, oppositely,
+## is the least conservative. 
 
 
 #######################
+
 ### PART 3: 2-way ANOVA
+
 #######################
+
 # Now we want to analyze the influence of 2 categorial variables (period and warning) on the speed.
+
 # So let's turn back to our initial dataset amis (not its subset with warning==1)
+
 # First, we need to again average the speed over each `pair`, `warning` and `period`.
+
+data.2way <- dcast(data, period + pair + warning ~ 'avg_speed', mean, na.rm = T, value.var = "speed")
+
 
 
 # Calculate the mean for each of 6 pairs of `period` and `warning`
 
+daa <- dcast(data.2way, period  + warning ~ 'mean', mean, na.rm = T, value.var = "avg_speed")
+
+
 
 # Do you think there is a significant difference in some of the groups?
 
+# It looks like there is very little differene in the groups. The most different groups are immediately after sign
+
+# before sign.
+
+
 
 # Now apply 2-way ANOVA: please use the function aov() on the speed depending on the period and warning
+
 # report p-value and interpret the result in details
+
+
+
+# We should use + between the variables because there is no interaction between them since before sign, the variable
+
+# perio doesn't make sense.
+
+aov2way <- aov(daa$mean ~ daa$period + daa$warning)
+
+summary(aov2way)
+
+
+
+# p-value with respect to period variable is 0.2444 which is not significant which means it doesn't depend when
+
+# the data is taken before/immediately/after the sign, it doesn't have significant affect on driver's speed
+
+
+
+# p-value with respect to warning variable is 0.0402 which is < 0.05 which means that the effect of presence of
+
+# warning sign makes significant difference
+
 
 
 # What do you conclude about the behaviour of drivers based on the 2-way ANOVA?
 
+# Based on 2-way ANOVA, we can conclude that the warning variable i.e. the presence of sign makes a significant
+
+# difference in drivers speed while the period variable i.e. when the data was taken before/immediately/after
+
+# the sign doesn't make a significant difference which is intuitive.
 
 
